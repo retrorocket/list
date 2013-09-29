bdef0101598daf870326cb2a42e4ea611bd0a022
your <you@example.com>
2013-09-29 14:19:21 +0900

    create_allがクソすぎるので改修

--- list.pl
+++ list.pl
@@ -4,20 +4,27 @@ use strict;
 use warnings;
 use utf8;
 
+#use KCatch;
 use Net::Twitter::Lite::WithAPIv1_1;
 use Mojolicious::Lite;
+#use Data::Dumper;
+use List::Compare;
 
 my $consumer_key ="***";
 my $consumer_secret = "***";
 
+#my $nt = Net::Twitter->new(
 my $nt = Net::Twitter::Lite::WithAPIv1_1->new(
 	consumer_key => $consumer_key,
 	consumer_secret => $consumer_secret,
+#	legacy_lists_api => 0
 );
 
 # Display top page
 get '/' => sub {
 	my $self = shift;
+	#$self->session( expires => 1 );
+	#$self->render;
 	my $access_token = $self->session( 'access_token' ) || '';
 	my $access_token_secret = $self->session( 'access_token_secret' ) || '';
 	my $screen_name = $self->session( 'screen_name' ) || '';
@@ -43,44 +50,82 @@ post '/list' => sub {
 	$nt->access_token( $access_token );
 	$nt->access_token_secret( $access_token_secret );
 
+	eval {
+
 	my $list = $nt->create_list({name=>'home_timeline_copy', mode=>'private'});
 	my $num = $list->{id_str};
 	my $hash = $nt->friends_ids({count=>4999});
 	my @mem = @{$hash->{ids}};
 
-	my $magic = 50;
-	my $count = @mem;
-	#print $count;
-	my $hyaku = int($count / $magic);
-	my $amari = $count % $magic;
-	eval {
+	my $only_count = -1;
+	$nt->add_list_member({list_id=>$num, screen_name=>$screen_name});
+
+	while ($only_count != 0) {
+
+		my $magic = 100;
+		my $count = @mem;
+
+		my $hyaku = int($count / $magic);
+		my $amari = $count % $magic;
 
 		my $i = 0;
 		for($i = 0; $i < $hyaku; $i++){
-			my @temp = @mem[$i*$magic ... (($i+1)*$magic)-1];
-			my $str = join(',', @temp);
-			#print $str;
-			$nt->add_list_members({list_id=>$num, user_id=>$str});
-			sleep 1;
+			eval {
+				my @temp = @mem[$i*$magic ... (($i+1)*$magic)-1];
+				my $str = join(',', @temp);
+				$nt->add_list_members({list_id=>$num, user_id=>$str});
+				sleep 1;
+			};
+		}
+		if($amari > 0){
+			eval {
+				my @temp = @mem[$hyaku*$magic ... $hyaku*$magic+($amari-1)];
+				my $str = join(',', @temp);
+				#print $str;
+				$nt->add_list_members({list_id=>$num, user_id=>$str});
+				sleep 1;
+			};
+			#if($@){
+				#$self->session( expires => 1 );
+				#return $self->render_json({'result' => $@ . "：処理途中で終了しました"});
+			#}
 		}
 
-		if($amari > 0){
-			my @temp = @mem[$hyaku*$magic ... $hyaku*$magic+($amari-1)];
-			my $str = join(',', @temp);
-			#print $str;
-			$nt->add_list_members({list_id=>$num, user_id=>$str});
+		#リストに登録できた勢
+		my $cursor = -1;
+		my $list_members = $nt->list_members({list_id => $num, cursor => $cursor});
+
+		my @list_mem;
+		for my $a (@{$list_members->{users}}){
+			push(@list_mem,$a->{id});
 		}
-		$nt->add_list_member({list_id=>$num, screen_name=>$screen_name});
+		$cursor = $list_members->{next_cursor};
+		while ($cursor != 0){
+			$list_members = $nt->list_members({list_id => $num, cursor => $cursor});
+			for my $a (@{$list_members->{users}}){
+				push(@list_mem,$a->{id});
+			}
+			$cursor = $list_members->{next_cursor};
+		}
+
+		my $lc = List::Compare->new(\@mem, \@list_mem);
+		my @only = $lc->get_Lonly;
+
+		$only_count = @only;
+
+		@mem = ();
+		@mem = @only;
+	}
 	};
 	if($@){
 		$self->session( expires => 1 );
-		return  $self->render_json({'result' => $@ . "：処理途中で終了しました"});
+		return $self->render_json({'result' => $@ . "：エラーが発生しました"});
 	}
+
 	$self->session( expires => 1 );
 	return  $self->render_json({'result' => "処理が完了しました"});
 	#セッション削除
 	#$self->render;
-
 } => 'list';
 
 # Session削除
