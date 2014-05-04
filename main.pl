08beb60a34ad5c4f04d69c9173121221df461762
your <you@example.com>
2014-05-05 01:27:33 +0900

    existing list

--- main.pl
+++ main.pl
@@ -3,6 +3,7 @@
 use strict;
 use warnings;
 use utf8;
+#use bigint;
 
 use Scalar::Util 'blessed';
 use Net::Twitter::Lite::WithAPIv1_1;
@@ -16,16 +17,17 @@ plugin 'mail';
 
 app->config(hypnotoad => {listen => ['http://*:secret']});
 app->hook('before_dispatch' => sub {
-	my $self = shift;
-	if ($self->req->headers->header('X-Forwarded-Host')) {
-		my $path = shift @{$self->req->url->path->parts};
-		push @{$self->req->url->base->path->parts}, $path;
-	}
-});
+		my $self = shift;
+		if ($self->req->headers->header('X-Forwarded-Host')) {
+			#Proxy Path setting
+			my $path = shift @{$self->req->url->path->parts};
+			push @{$self->req->url->base->path->parts}, $path;
+		}
+	});
 
 my $consumer_key ="***";
 my $consumer_secret = "***";
-my $from_address = "xxx@retrorocket.biz";
+my $from_address = "xxx\@retrorocket.biz";
 
 my $nt = Net::Twitter::Lite::WithAPIv1_1->new(
 	consumer_key => $consumer_key,
@@ -33,7 +35,47 @@ my $nt = Net::Twitter::Lite::WithAPIv1_1->new(
 	ssl => 1
 );
 
+sub getFaultedMember {
+	my $list_id = shift;
+	my $filename = shift;
+	my $mem = shift;
+	my $repeat_count = shift;
 
+	my $cursor = -1;
+	my $list_members = $nt->list_members({list_id => $list_id, cursor => $cursor});
+
+	my @list_mem;
+	for my $a (@{$list_members->{users}}){
+		push(@list_mem,$a->{id_str});
+	}
+	$cursor = $list_members->{next_cursor};
+	while ($cursor != 0){
+		$list_members = $nt->list_members({list_id => $list_id, cursor => $cursor});
+		for my $a (@{$list_members->{users}}){
+			push(@list_mem,$a->{id_str});
+		}
+		$cursor = $list_members->{next_cursor};
+	}
+
+	my $lc = List::Compare->new($mem, \@list_mem);
+	my @only = $lc->get_Lonly; #登録失敗者リスト
+
+	my $add_faulted_count = @only;
+	#JSON出力
+	my $mem_count = @list_mem;
+	open(OUT, ">$filename");
+		my %trig = ();
+		$trig{complete} = 0;
+		$trig{repeat} = $repeat_count;
+		$trig{result} = "Processing on ".$mem_count." members (left : ".$add_faulted_count." members)";
+		my $json_out = encode_json(\%trig);
+		print OUT $json_out;
+	close(OUT);
+
+	return @only;
+}
+
+# Display top page
 get '/' => sub {
 	my $self = shift;
 
@@ -41,12 +83,16 @@ get '/' => sub {
 	my $access_token_secret = $self->session( 'access_token_secret' ) || '';
 	my $screen_name = $self->session( 'screen_name' ) || '';
 
-	#セッションにトークンがないならトップに戻す
+	#セッションにトークンが残っていない
 	return $self->redirect_to( 'http://retrorocket.biz/list' ) unless ($access_token && $access_token_secret);
 
 	$self->stash('name' => $screen_name);
 
-	my $mail_mode = $self->session('mail') || ''; 
+	my $mail_mode = $self->session('mail') || '';
+	my $diff_mode = $self->session('diff') || '';
+
+	if($mail_mode && $diff_mode) {return $self->render('diff_mail');}
+	if($diff_mode) {return $self->render('diff');}
 	if($mail_mode) {return $self->render('mail');}
 
 } => 'index';
@@ -56,9 +102,11 @@ get '/auth' => sub {
 	my $self = shift;
 	my $mode = $self->param('mode') || '';
 	my $mail = $self->param('mail') || '';
+	my $diff = $self->param('diff') || '';
 
 	$self->session( mode => $mode );
 	$self->session( mail => $mail );
+	$self->session( diff => $diff );
 
 	my $cb_url = $self->url_for('auth_cb')->to_abs->scheme('https');
 	my $url = $nt->get_authorization_url( callback => $cb_url );
@@ -79,9 +127,11 @@ get '/auth_cb' => sub {
 	$nt->request_token( $token );
 	$nt->request_token_secret( $token_secret );
 
+	# Access token取得
 	my ($access_token, $access_token_secret, $user_id, $screen_name)
-	= $nt->request_access_token( verifier => $verifier );
+		= $nt->request_access_token( verifier => $verifier );
 
+	# セッションに格納
 	$self->session( access_token => $access_token );
 	$self->session( access_token_secret => $access_token_secret );
 	$self->session( screen_name => $screen_name );
@@ -89,25 +139,67 @@ get '/auth_cb' => sub {
 	$self->redirect_to( $self->url_for('index')->to_abs->scheme('https') );
 } => 'auth_cb';
 
+get '/list_get' => sub {
+	my $self = shift;
+	my $access_token = $self->session( 'access_token' ) || '';
+	my $access_token_secret = $self->session( 'access_token_secret' ) || '';
+	my $screen_name = $self->session( 'screen_name' ) || '';
+	return $self->render(json =>{'result' => "fault", 'complete' => -1}) unless ($access_token && $access_token_secret);
+
+	$nt->access_token( $access_token );
+	$nt->access_token_secret( $access_token_secret );
+
+	eval{	
+		my $lists = $nt->get_lists();
+		my $lists_num = @$lists;
+		if($lists_num == 0) { return $self->render(json =>{'result' => "fault", 'complete' => -1}); }
+
+		my @get_list=();
+		foreach my $elem (@$lists){
+			my %trig = ();
+			$trig{list_name} = $elem->{name};
+			$trig{list_id} = $elem->{id_str};
+			push(@get_list,\%trig);
+		}
+		return $self->render(json =>{'result' => "complete", 'complete' => \@get_list});
+	};
+	if($@){
+		return $self->render(json =>{'result' => "fault", 'complete' => -1});
+	}
+
+} => 'list_get';
+
 post '/list' => sub {
 
 	my $self = shift;
 
+	#Twitter API
 	my $access_token = $self->session( 'access_token' ) || '';
 	my $access_token_secret = $self->session( 'access_token_secret' ) || '';
 	my $screen_name = $self->session( 'screen_name' ) || '';
 
+	#各種モード切り替え
 	my $mode = $self->session('mode') || '';
 	my $sender_address = $self->param('address') || 'false';
 	my $mail_flag = $sender_address ne "false" ? 1 : 0;
+
+	#メール用メッセージ
+	my $MAIL_FAULT_TITLE = "TimeLine Copier Result (faulted)";
+	my $MAIL_FAULT_MESSAGE = "TimeLine Copierの処理に失敗しました。";
+
+	my $MAIL_PROCESS_TITLE = "TimeLine Copier Result (on process)";
+	my $MAIL_PROCESS_MESSAGE = "TimeLine Copierの処理が中断されました。15分後に処理を再開します。";
+
+	my $MAIL_SUCCEED_TITLE = "TimeLine Copier Result (succeed)'";
+	my $MAIL_SUCCEED_MESSAGE = "TimeLine Copierの処理が完了しました。Twitterを確認して下さい。";
 
 	#トークンなし
 	unless ($access_token && $access_token_secret) {
 		if($mail_flag == 1){
 			$self->mail(
 				to      => $sender_address,
-				subject => 'TimeLine Copier Result（fault）',
-				data    => "TimeLine Copierの処理に失敗しました。：Twitterに認証されていません。" ,
+				subject => $MAIL_FAULT_TITLE,
+				data    => $MAIL_FAULT_MESSAGE . "：Twitterに認証されていません。" ,
 				from    => $from_address
 			);
 		}
@@ -120,26 +212,16 @@ post '/list' => sub {
 	$nt->access_token( $access_token );
 	$nt->access_token_secret( $access_token_secret );
 
-
-	my $filename;
-	my $num;
-	my $list;
-	my $repeat = 0;
-	my $only_count = -1;
-	my @mem = ();
-	my $temp_only = -1;
-	my $temp_repeat = 0;
-	my $repeat_magic = 2;
-
-
-	$filename = app->home. "/public/".$screen_name.".json";
+	#JSON出力用パス＆処理開始前判定
+	my $filename = app->home. "/public/".$screen_name.".json";
 	if( -f $filename ) {
 		return $self->render(json =>{'result' => "fault", 'complete' => -2});
 		exit;
 	}
-	
+
 	my $pid = fork();
 	die "fork fault: $!" unless defined $pid;
+
 
 	if($pid) {
 		# 親プロセス
@@ -151,162 +233,161 @@ post '/list' => sub {
 	else {
 		#子プロセス
 		setsid;
-		
+
 		open (STDIN, "</dev/null");
 		open (STDOUT, ">/dev/null");
 		open (STDERR, ">&STDOUT");
 
+		#処理開始JSON出力
 		open(OUT, ">$filename");
-		my %trig = ();
-		$trig{complete} = 0;
-		$trig{result} = "Processing ...";
-		my $json_out = encode_json(\%trig);
-		print OUT $json_out;
+			my %trig = ();
+			$trig{complete} = 0;
+			$trig{result} = "Processing ...";
+			my $json_out = encode_json(\%trig);
+			print OUT $json_out;
 		close(OUT);
 
-		do {
-			eval {
-				if($repeat == 0) {
-					$list = $nt->create_list({name=>'home_timeline_copy', mode=>'private', description =>"It\'s processing." });
-					$num = $list->{id_str};
-					sleep 2;
-				
-					my $hash;
-					if($mode eq 'follower'){
-						$hash = $nt->followers_ids({count=>4999, stringify_ids =>'true'});
-					}
-					else {
-						$hash = $nt->friends_ids({count=>4999, stringify_ids=>'true'});
-					}
-					@mem = @{$hash->{ids}};
+		#my $list;
+		my $list_id; #リストID
+		my @mem = (); #リスト登録対象
 
-					$nt->add_list_member({list_id=>$num, screen_name=>$screen_name});
-				}
+		eval{
 
-				while ($only_count != 0 && $temp_repeat < $repeat_magic) {
-
-					$temp_only = $only_count;
-	
-					my $magic = 50;
-					my $count = @mem;
-
-					my $hyaku = int($count / $magic);
-					my $amari = $count % $magic;
-
-					my $i = 0;
-					for($i = 0; $i < $hyaku; $i++){
-						eval {
-							my @temp = @mem[$i*$magic ... (($i+1)*$magic)-1];
-							my $str = join(',', @temp);
-							$nt->add_list_members({list_id=>$num, user_id=>$str});
-							sleep 1;
-						};
-					}
-					if($amari > 0){
-						eval {
-							my @temp = @mem[$hyaku*$magic ... $hyaku*$magic+($amari-1)];
-							my $str = join(',', @temp);
-							#print $str;
-							$nt->add_list_members({list_id=>$num, user_id=>$str});
-							sleep 1;
-						};
-					}
-
-				#リストに登録できた勢
-					my $cursor = -1;
-					my $list_members = $nt->list_members({list_id => $num, cursor => $cursor});
-
-					my @list_mem;
-					for my $a (@{$list_members->{users}}){
-						push(@list_mem,$a->{id_str});
-					}
-					$cursor = $list_members->{next_cursor};
-					while ($cursor != 0){
-						$list_members = $nt->list_members({list_id => $num, cursor => $cursor});
-						for my $a (@{$list_members->{users}}){
-							push(@list_mem,$a->{id_str});
-						}
-						$cursor = $list_members->{next_cursor};
-					}
-
-					my $lc = List::Compare->new(\@mem, \@list_mem);
-					my @only = $lc->get_Lonly;
-
-					$only_count = @only;
-					if($only_count >= $temp_only) {$temp_repeat++;}
-					
-					@mem = ();
-					@mem = @only;
-
-					my $mem_count = @list_mem;
-					open(OUT, ">$filename");
-					my %trig = ();
-					$trig{complete} = 0;
-					$trig{repeat} = $temp_repeat;
-					$trig{result} = "Processing on ".$mem_count." members (left : ".$only_count." members)";
-					my $json_out = encode_json(\%trig);
-					print OUT $json_out;
-					close(OUT);
-	
-				}
-			};
-			if( my $err = $@ ){
-				die $@ unless blessed $err && $err->isa('Net::Twitter::Lite::Error');
-				my $error = $err->code;
-				$repeat++;
-				if($mail_flag == 1 && $repeat == 1 && $error != 404){
-					$self->mail(
-						to      => $sender_address,
-						subject => 'TimeLine Copier Result (on process)',
-						data    => "TimeLine Copierの処理が中断されました。15分後に処理を再開します。\n".$@ ,
-						from    => $from_address
-					);
-					sleep 900;
-				}
-				elsif ( ($mail_flag == 1 && $repeat > 1) || ($mail_flag == 1 && $error == 404) ) {
-					$self->mail(
-						to      => $sender_address,
-						subject => 'TimeLine Copier Result (faulted)',
-						data    => "TimeLine Copierの処理に失敗しました。\n".$@ ,
-						from    => $from_address
-					);
-					unlink($filename);
-					if($error != 404) {
-						$nt->update_list({list_id => $num, description => "error : ".$@});
-					}
-					#$self->session( expires => 1 );
-					close (SSTDIN);
-					close (STDOUT);
-					close (STDERR);
-					exit;
-				}
-				else {
-					unlink($filename);
-					if($error != 404) {
-						$nt->update_list({list_id => $num, description => "error : ".$@});
-					}
-					#しなくてもいいんだけど
-					close (SSTDIN);
-					close (STDOUT);
-					close (STDERR);
-					exit;
-				}
+			my $hash;
+			if($mode eq 'follower'){
+				$hash = $nt->followers_ids({count=>4999, stringify_ids =>'true'});
 			}
-		} while($mail_flag == 1 && $repeat == 1);
+			else {
+				$hash = $nt->friends_ids({count=>4999, stringify_ids=>'true'});
+			}
+			@mem = @{$hash->{ids}};
 
-		if($temp_repeat >= $repeat_magic) {
+			$list_id = $self->param('list_id') || '';
+			if( $list_id ){
+				my @only = &getFaultedMember($list_id, $filename, \@mem, 0);
+				sleep 1;
+				@mem = ();
+				@mem = @only;
+				$nt->update_list({list_id => $list_id, description => "It\'s processing." });
+				sleep 2;
+			}
+			else{
+				my $list = $nt->create_list({name=>'home_timeline_copy', mode=>'private', description =>"It\'s processing." });
+				$list_id = $list->{id_str};
+				sleep 2;
+			}
+
+			#自分を入れておく
+			$nt->add_list_member({list_id=>$list_id, screen_name=>$screen_name});
+		};
+		if($@){
 			if($mail_flag == 1){
-
 				$self->mail(
 					to      => $sender_address,
-					subject => 'TimeLine Copier Result (faulted)',
-					data    => $only_count."名の存在しない・凍結された可能性のあるユーザをリストに登録できませんでした。Twitterを確認して下さい。" ,
+					subject => $MAIL_FAULT_TITLE,
+					data    => $MAIL_FAULT_MESSAGE."\n".$@ ,
 					from    => $from_address
 				);
 			}
 			unlink($filename);
-			$nt->update_list({list_id => $num, description => $only_count."名の存在しない・凍結された可能性のあるユーザをリストに登録できませんでした"});
+			close (SSTDIN);
+			close (STDOUT);
+			close (STDERR);
+			exit;
+		}
 
+		my $error_count = 0; #APIエラー発生回数
+
+		my $add_faulted_count = -1; #リスト登録に失敗した人数
+		my $repeat_count = 0; #ループ回数
+		my $REPEAT_MAGIC = 2; #この回数以上リピートしたらループを切る
+
+		while ($add_faulted_count != 0 && $repeat_count < $REPEAT_MAGIC) {
+			eval {
+				my $temp_add_faulted_count = $add_faulted_count;
+
+				my $magic = 50; #一度にリストにぶち込む人数
+				my $count = @mem;
+
+				my $hyaku = int($count / $magic);
+				my $amari = $count % $magic;
+
+				#割り切れる分の処理
+				for(my $i = 0; $i < $hyaku; $i++){
+					eval {
+						my @temp = @mem[$i*$magic ... (($i+1)*$magic)-1];
+						my $str = join(',', @temp);
+						$nt->add_list_members({list_id=>$list_id, user_id=>$str});
+						sleep 1;
+					};
+				}
+				#端数
+				if($amari > 0){
+					eval {
+						my @temp = @mem[$hyaku*$magic ... $hyaku*$magic+($amari-1)];
+						my $str = join(',', @temp);
+						#print $str;
+						$nt->add_list_members({list_id=>$list_id, user_id=>$str});
+						sleep 1;
+					};
+				}
+
+				#リストに登録できた勢を計算する
+				my @only = &getFaultedMember($list_id, $filename, \@mem, $repeat_count);
+				$add_faulted_count = @only;
+				if($add_faulted_count >= $temp_add_faulted_count) {$repeat_count++;}
+				@mem = ();
+				@mem = @only;
+			};
+			if( my $err = $@ ){
+				die $@ unless blessed $err && $err->isa('Net::Twitter::Lite::Error');
+				my $error = $err->code;
+				$error_count++;
+
+				#一回目のエラー、かつ404じゃないなら再開する
+				if($mail_flag == 1 && $error_count == 1 && $error != 404){
+					$self->mail(
+						to      => $sender_address,
+						subject => $MAIL_PROCESS_TITLE,
+						data    => $MAIL_PROCESS_MESSAGE."\n".$@ ,
+						from    => $from_address
+					);
+					sleep 900;
+					next;
+				}
+				
+				#失敗しました
+				if($mail_flag == 1) {
+					$self->mail(
+						to      => $sender_address,
+						subject => $MAIL_FAULT_TITLE,
+						data    => $MAIL_FAULT_MESSAGE ."\n".$@ ,
+						from    => $from_address
+					);
+				}
+				$nt->update_list({list_id => $list_id, description => "error : ".$@});
+				#$self->session( expires => 1 );
+				unlink($filename);
+				close (SSTDIN);
+				close (STDOUT);
+				close (STDERR);
+				exit;
+			}
+		}
+
+		#無限ループしそうになった
+		if($repeat_count >= $REPEAT_MAGIC) {
+			if($mail_flag == 1){
+				$self->mail(
+					to      => $sender_address,
+					subject => $MAIL_FAULT_TITLE,
+					data    => $add_faulted_count."名の存在しない・凍結された可能性のあるユーザをリストに登録できませんでした。Twitterを確認して下さい。" ,
+					from    => $from_address
+				);
+			}
+			$nt->update_list({list_id => $list_id, description => $add_faulted_count."名の存在しない・凍結された可能性のあるユーザをリストに登録できませんでした"});
+
+			unlink($filename);
 			close (SSTDIN);
 			close (STDOUT);
 			close (STDERR);
@@ -314,17 +395,16 @@ post '/list' => sub {
 		}
 
 		if($mail_flag == 1){
-
 			$self->mail(
 				to      => $sender_address,
-				subject => 'TimeLine Copier Result (succeed)',
-				data    => "TimeLine Copierの処理が完了しました。Twitterを確認して下さい。" ,
+				subject => $MAIL_SUCCEED_TITLE,
+				data    => $MAIL_SUCCEED_MESSAGE ,
 				from    => $from_address
 			);
 		}
-		unlink($filename);
-		$nt->update_list({list_id => $num, description => "succeed"});
+		$nt->update_list({list_id => $list_id, description => "succeed"});
 
+		unlink($filename);
 		close (SSTDIN);
 		close (STDOUT);
 		close (STDERR);
@@ -333,11 +413,12 @@ post '/list' => sub {
 
 } => 'list';
 
-# セッション消去（隠しオプション）
+# セッション消去
 get '/logout' => sub {
 	my $self = shift;
 	$self->session( expires => 1 );
 	#$self->render;
+	return $self->render(json =>{'result' => "logout"});
 } => 'logout';
 
 app->sessions->secure(1);
